6295fb332ff6cd634dfe5a43e4a49efc526d0ad4
kaheiseki <kaheiseki.code@gmail.com>
2022-03-19 22:36:26 +0900

    二つの関数の機能を修正

--- itaizi/itaizi.py
+++ itaizi/itaizi.py
@@ -1,16 +1,23 @@
 from .list import itaizi_list
 
-def to_seizi(char):
-  if char in itaizi_list:
-    return itaizi_list(char)
-  else:
-    return char
+# 文字列中の異体字を正字に変換したものを返す関数
+def to_seizi(string):
+  text = ""
+  for char in string:
+    if char in itaizi_list:
+      text += itaizi_list(char)
+    else:
+      text += char
+  return text
   
-
-def to_itaizi(char):
-  if char in itaizi_list.values():
-    for key,value in itaizi_list.items():
-      if value == char:
-        return key
-  else:
-    return char
+# 文字列中の正字をい異体字に変換したものを返す関数
+def to_itaizi(string):
+  text = ""
+  for char in string:
+    if char in itaizi_list.values():
+      for key,value in itaizi_list.items():
+        if value == char:
+          text += key
+    else:
+      text += char
+  return text 
