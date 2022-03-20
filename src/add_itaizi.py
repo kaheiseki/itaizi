a514b8d7efef41419aae4978a9c48ab1a4f8bf31
kaheiseki <kaheiseki.code@gmail.com>
2022-03-21 01:08:44 +0900

    itaizi_listの追加用のソースの追加

--- src/add_itaizi.py
+++ src/add_itaizi.py
@@ -1,19 +1,20 @@
+from socket import SO_VM_SOCKETS_BUFFER_MIN_SIZE
 import sys
 sys.dont_write_bytecode  = True
 sys.path.append("../itaizi")
 from list import itaizi_list
 
 # TODO keyとvalueを持ってくる
-key = ["hoge","hoge"]
-value = ["hoge","hoge"]
+itaizi = ["hoge","hoge"]
+seizi = ["hoge","hoge"]
 
 
 # 本番ではpathをsample.pyからlist.pyに変える
 with open("../itaizi/sample.py",mode = "w") as f:
   f.write("itaizi_list = {\n")
-  for index in range(len(key)):
-    if index != len(key) - 1:
-      f.write("  \"" + key[index] + "\":\"" + value[index] + "\",\n")
-    elif index == len(key) - 1:
-      f.write("  \"" + key[index] + "\":\"" + value[index] + "\"\n") 
+  for index in range(len(itaizi)):
+    if index != len(itaizi) - 1:
+      f.write("  \"" + itaizi[index] + "\":\"" + seizi[index] + "\",\n")
+    elif index == len(itaizi) - 1:
+      f.write("  \"" + seizi[index] + "\":\"" + seizi[index] + "\"\n") 
   f.write("}")
